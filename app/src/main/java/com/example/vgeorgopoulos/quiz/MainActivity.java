6cd5c4bf17d213e692c7ec76b6c933b93f519636
vgeorgopoulos <vgeorgopoulos@outlook.com>
2016-11-01 23:01:22 +0200

    final

--- app/src/main/java/com/example/vgeorgopoulos/quiz/MainActivity.java
+++ app/src/main/java/com/example/vgeorgopoulos/quiz/MainActivity.java
@@ -1,58 +1,157 @@
 package com.example.vgeorgopoulos.quiz;
 
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private int score = 0;
-    private RadioButton firstCorrect = (RadioButton) findViewById(R.id.answerOneC);
-    private RadioButton secondCorrect = (RadioButton) findViewById(R.id.answerTwoB);
-    private RadioButton thirdCorrect = (RadioButton) findViewById(R.id.answerThreeD);
-    private RadioButton fourthCorrect = (RadioButton) findViewById(R.id.answerFourB);
-    private RadioButton fifthCorrect = (RadioButton) findViewById(R.id.answerFiveC);
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
+    /**
+     * This is called when the Submit button is clicked
+     */
     public void quizResult(View view) {
-        showCorrect();
-        calculateScore();
+        firstQuestion();
+        secondQuestion();
+        thirdQuestion();
+        fourthQuestion();
+        fifthQuestion();
+        sixthQuestion();
+        seventhQuestion();
+        eighthQuestion();
+        ninenthQuestion();
+        String toastMessage = String.format("Your final Score is: %d/9", score);
+        Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show();
     }
 
-    private void showCorrect() {
+    /**
+     * This method checks if the correct RadioButton is checked and adds to the score,
+     * also turns the colour of the text for the correct Answer to Green.
+     */
+    private void firstQuestion() {
+        RadioButton firstCorrect = (RadioButton) findViewById(R.id.answerOneC);
         firstCorrect.setTextColor(Color.GREEN);
-        secondCorrect.setTextColor(Color.GREEN);
-        thirdCorrect.setTextColor(Color.GREEN);
-        fourthCorrect.setTextColor(Color.GREEN);
-        fifthCorrect.setTextColor(Color.GREEN);
-    }
-
-    private void calculateScore() {
         if (firstCorrect.isChecked()) {
             score += 1;
         }
+    }
+
+    /**
+     * This method checks if the correct RadioButton is checked and adds to the score,
+     * also turns the colour of the text for the correct Answer to Green.
+     */
+    private void secondQuestion() {
+        RadioButton secondCorrect = (RadioButton) findViewById(R.id.answerTwoC);
+        secondCorrect.setTextColor(Color.GREEN);
         if (secondCorrect.isChecked()) {
             score += 1;
         }
+    }
+
+    /**
+     * This method checks if the correct RadioButton is checked and adds to the score,
+     * also turns the colour of the text for the correct Answer to Green.
+     */
+    private void thirdQuestion() {
+        RadioButton thirdCorrect = (RadioButton) findViewById(R.id.answerThreeC);
+        thirdCorrect.setTextColor(Color.GREEN);
         if (thirdCorrect.isChecked()) {
             score += 1;
         }
+    }
+
+    /**
+     * This method checks if the correct RadioButton is checked and adds to the score,
+     * also turns the colour of the text for the correct Answer to Green.
+     */
+    private void fourthQuestion() {
+        RadioButton fourthCorrect = (RadioButton) findViewById(R.id.answerFourC);
+        fourthCorrect.setTextColor(Color.GREEN);
         if (fourthCorrect.isChecked()) {
             score += 1;
         }
+    }
+
+    /**
+     * This method checks if the correct RadioButton is checked and adds to the score,
+     * also turns the colour of the text for the correct Answer to Green.
+     */
+    private void fifthQuestion() {
+        RadioButton fifthCorrect = (RadioButton) findViewById(R.id.answerFiveC);
+        fifthCorrect.setTextColor(Color.GREEN);
         if (fifthCorrect.isChecked()) {
             score += 1;
         }
+    }
+
+    /**
+     * This method checks if the text entered from the user is correct and adds to the score
+     */
+    private void sixthQuestion() {
+        EditText sixthEditText = (EditText) findViewById(R.id.answerSixEdit);
+        String sixthCorrect = sixthEditText.getText().toString().toUpperCase();
+        if (sixthCorrect.equals("C") || sixthCorrect.equals("C.") || sixthCorrect.equals("SCARFACE")) {
+            score += 1;
+        }
+    }
+
+    /**
+     * This method checks if the text entered from the user is correct and adds to the score
+     */
+    private void seventhQuestion() {
+        EditText seventhEditText = (EditText) findViewById(R.id.answerSevenEdit);
+        String seventhCorrect = seventhEditText.getText().toString().toUpperCase();
+        if (seventhCorrect.equals("D") || seventhCorrect.equals("D.") || seventhCorrect.equals("SAVING PRIVATE RYAN")) {
+            score += 1;
+        }
+    }
+
+    /**
+     * This method checks if the text entered from the user is correct and adds to the score
+     */
+    private void eighthQuestion() {
+        EditText eighthEditText = (EditText) findViewById(R.id.answerEightEdit);
+        String eighthCorrect = eighthEditText.getText().toString().toUpperCase();
+        if (eighthCorrect.equals("C") || eighthCorrect.equals("C.") || eighthCorrect.equals("SCENT OF A WOMAN")) {
+            score += 1;
+        }
+    }
+
+    /**
+     * Checks which CheckBoxes are checked and adds to the score if correct
+     */
+    private void ninenthQuestion() {
+        CheckBox firstBox = (CheckBox) findViewById(R.id.answerNineA);
+        CheckBox secondBox = (CheckBox) findViewById(R.id.answerNineB);
+        CheckBox thirdBox = (CheckBox) findViewById(R.id.answerNineC);
+        CheckBox fourthBox = (CheckBox) findViewById(R.id.answerNineD);
+        firstBox.setTextColor(Color.GREEN);
+        secondBox.setTextColor(Color.GREEN);
+        thirdBox.setTextColor(Color.GREEN);
+        fourthBox.setTextColor(Color.RED);
+        if (firstBox.isChecked() && secondBox.isChecked() && thirdBox.isChecked() && thirdBox.isChecked() == false) {
+
+            score += 1;
+        }
+
 
     }
+
 }
 
 
